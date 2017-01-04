fe94305408f7044198a472433877918a16ee1423
santiago_762000@yahoo.com <santiago_762000@yahoo.com>
2017-01-04 14:20:14 +1000

    Simple Camera Example

---
+++ android/Camera/app/src/main/java/com/example/javier/camera/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.javier.camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv=(ImageView)findViewById(R.id.imageView2);
+
+
+    }
+    public void buttonClick(View v){
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+        iv.setImageBitmap(bitmap);
+
+    }
+}
